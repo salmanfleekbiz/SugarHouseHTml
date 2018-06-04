18010c026ea5df6c8f658790b48bcc3e2207b523
salmanfleekbiz <35482716+salmanfleekbiz@users.noreply.github.com>
2018-06-04 14:08:12 +0545

    First initialization
    Salman Commit

---
+++ assest/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+
+$("#phone").intlTelInput();
+
+
+$('.navbar-toggle').click(function(){
+	$('#navbar-collapse-1').removeClass('collapse');
+	$('#navbar-collapse-1').toggleClass('fixed');
+})
+
+
+
+});
